3bc4cdc710c93ef5208765595ebdfc865417023d
sam-ng <ngs5@bxscience.edu>
2016-12-29 22:36:10 -0500

    Minesweeper Main Class File

---
+++ Minesweeper.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Minesweeper {
+  public static createEasyGrid() {
+    int[][] grid = new int[10][10];
+  }
+  public static createMediumGrid
+    
+  public static void main(String[] args) {
+    Scanner ms = new Scanner(System.in);
+    System.out.println("Enter the difficulty level: Easy, Medium, Hard, Impossible, or Custom.");
+    String diff = ms.nextLine();
+    
